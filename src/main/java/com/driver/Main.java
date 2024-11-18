1f86b726318de50041545efa95498e421a616a55
RUCHIR04-R <ruchirgawture@gmail.com>
2024-11-18 10:06:11 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -12,7 +12,7 @@ class B extends A {
 }
 public class Main {
   public static void main(String[] args) {
-        B objB = new B();
+      B objB = new B();
         System.out.println(objB.meth());
     }
 }
